b24eed6ac2ca0269ad894f9f8a4164df7f1dc7b3
ericdai713 <ericdai713@gmail.com>
2019-12-25 17:14:55 +0800

    Add files via upload

---
+++ make_submission.py
@@ -0,0 +1,106 @@
+import cv2
+import numpy as np
+import pandas as pd
+import threading
+import queue
+import tensorflow as tf
+from tqdm import tqdm
+from keras.models import load_model
+from keras.backend import set_session
+from losses import *
+
+WIDTH = 1280
+HEIGHT = 1280
+BATCH_SIZE = 4
+ORIG_WIDTH = 1918
+ORIG_HEIGHT = 1280
+global sess
+global graph
+
+def run_length_encode(mask):
+    inds = mask.flatten()
+    runs = np.where(inds[1:] != inds[:-1])[0] + 2
+    runs[1::2] = runs[1::2] - runs[:-1:2]
+    rle = ' '.join([str(r) for r in runs])
+    return rle
+
+
+df_test = pd.read_csv('input/sample_submission.csv')
+ids_test = df_test['img'].map(lambda s: s.split('.')[0])
+
+names = []
+for _id in ids_test:
+    names.append('{}.jpg'.format(_id))
+
+rles = []
+filepath = 'weights/1225_27.hdf5'
+
+cfg = tf.ConfigProto()
+sess = tf.Session(config = cfg)
+set_session(sess)
+
+model = load_model(
+    filepath,
+    custom_objects={'bce_dice_loss': bce_dice_loss, 'dice_coef': dice_coef}
+)
+
+graph = tf.get_default_graph()
+
+q_size = 10
+
+
+def data_loader(q, ):
+    for start in range(0, len(ids_test), BATCH_SIZE):
+        x_batch = []
+        
+        end = min(start + BATCH_SIZE, len(ids_test))
+        ids_test_batch = ids_test[start:end]
+        
+        for id in ids_test_batch.values:
+            img = cv2.imread('input/test_hq/{}.jpg'.format(id))
+            img = cv2.resize(img, (WIDTH, HEIGHT))
+            
+            x_batch.append(img)
+        
+        x_batch = np.array(x_batch, np.float32) / 255.0
+        q.put(x_batch)
+
+
+def predictor(q, ):
+    #ind=0
+    for _ in tqdm(range(0, len(ids_test), BATCH_SIZE)):
+        x_batch = q.get()
+        
+        with graph.as_default():
+            set_session(sess)
+            preds = model.predict_on_batch(x_batch)
+        
+        preds = np.squeeze(preds, axis=3)
+
+        #if(ind == 0):
+            #print(" [PRED] img=", x_batch)
+            #cv2.imwrite('maks.jpg', preds)
+        
+        for pred in preds:
+            prob = cv2.resize(pred, (ORIG_WIDTH, ORIG_HEIGHT))
+            #print(prob)
+            mask = prob > 0.5
+            rle = run_length_encode(mask)
+            rles.append(rle)
+        #if (ind > 5):
+        #    break
+        #ind = ind + 1
+
+q = queue.Queue(maxsize=q_size)
+#print("hi")
+t1 = threading.Thread(target=data_loader, name='DataLoader', args=(q,))
+t2 = threading.Thread(target=predictor, name='Predictor', args=(q,))
+#print("h2")
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
+
+df = pd.DataFrame({'img': names, 'rle_mask': rles})
+df.to_csv('submit/submission.csv.gz', index=False, compression='gzip')
